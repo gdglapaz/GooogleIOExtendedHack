6f94fc7d295cc4867e3f2e8296c0922a75a3d751
JuanSony <juan.c.c.q@gmail.com>
2013-05-18 16:00:16 -0400

    Agregango listado de participantes

--- app/gen/com/gdglapaz/gdgbo/R.java
+++ app/gen/com/gdglapaz/gdgbo/R.java
@@ -142,21 +142,22 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f04000f;
+        public static final int action_settings=0x7f040010;
         public static final int btn_launch_oauth=0x7f040007;
-        public static final int button1=0x7f04000e;
+        public static final int button1=0x7f04000f;
         public static final int directorio=0x7f040009;
         public static final int hybrid=0x7f040004;
         public static final int loginGPlus=0x7f040008;
         public static final int logo=0x7f040006;
+        public static final int nombre=0x7f04000a;
         public static final int none=0x7f040000;
         public static final int normal=0x7f040001;
         public static final int participantes=0x7f040005;
-        public static final int revoke_access_button=0x7f04000c;
+        public static final int revoke_access_button=0x7f04000d;
         public static final int satellite=0x7f040002;
-        public static final int sign_in_button=0x7f04000a;
-        public static final int sign_in_status=0x7f04000d;
-        public static final int sign_out_button=0x7f04000b;
+        public static final int sign_in_button=0x7f04000b;
+        public static final int sign_in_status=0x7f04000e;
+        public static final int sign_out_button=0x7f04000c;
         public static final int terrain=0x7f040003;
     }
     public static final class layout {
@@ -164,7 +165,8 @@ containing a value of this type.
         public static final int activity_main=0x7f030001;
         public static final int activity_menu=0x7f030002;
         public static final int activity_welcome=0x7f030003;
-        public static final int sign_in_activity=0x7f030004;
+        public static final int list_directory_item=0x7f030004;
+        public static final int sign_in_activity=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f080000;
--- app/src/com/gdglapaz/gdgbo/DirectoryListActivity.java
+++ app/src/com/gdglapaz/gdgbo/DirectoryListActivity.java
@@ -7,24 +7,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.androidhive.jsonparsing.R;
-import com.androidhive.jsonparsing.SingleMenuItemActivity;
-
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
-import android.widget.AdapterView.OnItemClickListener;
 
 public class DirectoryListActivity extends Activity {
-	private static String url = "http://gdglapaz.codigobase.com/";
+	private static String url = "http://gdglapaz.codigobase.com/index.php";
+	//private static String url = "http://www.cuencasbolivia.org/gapi/clients.json";
 	
-	private static final String TAG_CLIENTS = "clients";
+	private static final String TAG_CLIENTS = "listado";
 	private static final String TAG_ID = "id";
 	private static final String TAG_NAME = "name";
 	private static final String TAG_CUENTA = "cuenta";
@@ -76,16 +68,16 @@ public class DirectoryListActivity extends Activity {
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		/*
+		
 		ListAdapter adapter = new SimpleAdapter(this, contactList,
-				R.layout.list_item,
-				new String[] { TAG_NAME, TAG_SUCURSAL }, new int[] {
-						R.id.name, R.id.mobile });
+				R.layout.list_directory_item ,
+				new String[] { TAG_NAME}, new int[] {
+						R.id.nombre });
 
-		setListAdapter(adapter);
+		//setListAdapter(adapter);
 
 		// selecting single ListView item
-		ListView lv = getListView();*/
+		/*ListView lv = getListView();*/
 
 		// Launching new screen on Selecting Single ListItem
 		/*lv.setOnItemClickListener(new OnItemClickListener() {
